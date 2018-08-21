df13da9ad5ae75aa984751d3d7678b46b7f9103f
Nguyen Cong Binh <ngco.binh@gmail.com>
2018-08-21 20:20:12 +0700

    change root view

---
+++ Flange/Flange/Flange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flange
+//
+//  Created by PqThanh on 8/21/18.
+//  Copyright © 2018 pqt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
